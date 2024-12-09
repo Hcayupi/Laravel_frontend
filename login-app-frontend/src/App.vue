e051650aaaaa91ed802d0b5058c1b17350283a72
hcayupi <hipolito.cayupi@gmail.com>
2024-12-09 01:11:39 -0300

    Primer commit

---
+++ login-app-frontend/src/App.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import TheWelcome from "./components/TheWelcome.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    TheWelcome,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+
+<template>
+  <header>
+    <div id="app">
+      <h1>¡Hola, Vue con TypeScript!</h1>
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+h1 {
+  color: teal;
+}
+</style>
---
+++ login-app-frontend/src/components/TheWelcome.vue
@@ -0,0 +1,19 @@
+<!-- src/components/TheWelcome.vue -->
+<template>
+    <div>
+      <h2>¡Bienvenido a tu proyecto Vue + TypeScript!</h2>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    name: "TheWelcome",
+  };
+  </script>
+  
+  <style scoped>
+  h2 {
+    color: darkblue;
+  }
+  </style>
+  
